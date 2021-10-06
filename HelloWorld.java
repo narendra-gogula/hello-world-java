acef0e84b3d1a57302c89cdb40c5c8b03d06a341
narendra-gogula <91150108+narendra-gogula@users.noreply.github.com>
2021-10-06 15:36:06 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,7 +34,7 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, Everyone");
     System.out.println("Good Morning");
     System.out.println("This is Narendra Reddy Gogula");
   }
